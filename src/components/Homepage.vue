4a22c4d47e1d867eabbf0376171d53614a043bea
Jonathan Foucher <jonathan-foucher@outlook.fr>
2022-01-02 23:45:54 +0100

    start the Terminal component

---
+++ src/components/Homepage.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <p>
+      Actually building the website. Please come back later :)
+    </p>
+    <Terminal/>
+  </v-container>
+</template>
+
+<script>
+import Terminal from './Terminal';
+
+  export default {
+    name: 'Homepage',
+
+    components: {
+      Terminal,
+    },
+
+    data: () => ({}),
+  }
+</script>
