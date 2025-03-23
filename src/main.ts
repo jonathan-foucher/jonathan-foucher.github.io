cccc135b7b7bc6324d9bfc774acb6b2a893b90c3
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-03-24 00:38:59 +0100

    add pinia and flag-icons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 
 // Quasar
 import { Quasar } from 'quasar'
@@ -7,7 +7,15 @@ import quasarLang from 'quasar/lang/en-GB'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
 
+// flag-icons
+import 'flag-icons/css/flag-icons.min.css'
+
+// pinia store
+import { createPinia } from 'pinia'
+const pinia = createPinia()
+
 createApp(App)
+  .use(pinia)
   .use(Quasar, {
     plugins: {},
     lang: quasarLang,
