42baa74c7661068a592119cf4fda5d0fb29e9f86
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-03-23 20:47:41 +0100

    add husky to trigger prettier on pre commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,12 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
     quasar({
       sassVariables: fileURLToPath(
         new URL('./src/quasar-variables.sass', import.meta.url)
-      )
-    })
+      ),
+    }),
   ],
 })
