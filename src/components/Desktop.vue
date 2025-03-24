6cdaf6a4cc09b76dfaf65b6c67db0b2554389124
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-03-24 13:09:26 +0100

    add eslint

--- src/components/Desktop.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import Toolbar from '@/components/toolbar/Toolbar.vue'
-</script>
-
-<template>
-  <toolbar />
-</template>
---
+++ src/components/DesktopScreen.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import DesktopToolbar from '@/components/toolbar/DesktopToolbar.vue'
+</script>
+
+<template>
+  <desktop-toolbar />
+</template>
