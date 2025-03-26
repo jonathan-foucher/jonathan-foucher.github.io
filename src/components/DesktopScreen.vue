9c3780339e46e75814672646fb2c64d880a4dc9e
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-03-27 00:35:33 +0100

    add possibility to close apps

--- src/components/DesktopScreen.vue
+++ src/components/DesktopScreen.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import AppWindow from '@/components/AppWindow.vue'
+import type Application from '@/types/Application.ts'
+
+const openedApps = ref<Array<Application>>([{ name: 'some-app' }, { name: 'some-other-app' }])
+
+const closeApp = (name: string) => {
+  openedApps.value = openedApps.value.filter((app) => app.name !== name)
+}
 </script>
 
 <template>
-  <app-window />
+  <app-window v-for="app in openedApps" :key="app.name" :name="app.name" :close-app="closeApp" />
 </template>
