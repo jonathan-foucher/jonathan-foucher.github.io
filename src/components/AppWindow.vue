3aa74b1ffa04fc0f5b6e33610ceaaad2b1f2bc2b
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-03-26 23:06:55 +0100

    lock app windows on the desktop

--- src/components/AppWindow.vue
+++ src/components/AppWindow.vue
@@ -5,13 +5,35 @@ import { QPageContainer, type TouchPanValue } from 'quasar'
 const APP_TOOLBAR_SIZE_PX = 40
 const windowPosition = ref([10, APP_TOOLBAR_SIZE_PX + 10])
 const draggingWindow = ref(false)
+const windowRef = ref<InstanceType<typeof QPageContainer>>()
+
+const getNewPositionValue = (currentValue: number, delta: number, min: number, max: number) => {
+  const newValue = currentValue + delta
+  if (newValue < min) {
+    return min
+  }
+  if (newValue > max) {
+    return max
+  }
+  return newValue
+}
 
 const moveWindow: TouchPanValue = (event) => {
   draggingWindow.value = !event.isFirst && !event.isFinal
 
   windowPosition.value = [
-    windowPosition.value[0] + (event.delta?.x ?? 0),
-    windowPosition.value[1] + (event.delta?.y ?? 0),
+    getNewPositionValue(
+      windowPosition.value[0],
+      event.delta?.x ?? 0,
+      0,
+      document.body.offsetWidth - windowRef.value?.$el?.clientWidth
+    ),
+    getNewPositionValue(
+      windowPosition.value[1],
+      event.delta?.y ?? 0,
+      APP_TOOLBAR_SIZE_PX,
+      document.body.offsetHeight - windowRef.value?.$el?.clientHeight
+    ),
   ]
 }
 </script>
@@ -20,7 +42,7 @@ const moveWindow: TouchPanValue = (event) => {
   <q-layout class="disable-select cursor-default">
     <q-page-container>
       <q-slide-transition appear :duration="300">
-        <q-page-sticky position="top-left" :offset="windowPosition">
+        <q-page-sticky ref="windowRef" position="top-left" :offset="windowPosition">
           <q-bar dark class="bg-primary text-white" v-touch-pan.prevent.mouse="moveWindow">
             <q-btn dense flat round icon="lens" size="8.5px" color="red" />
             <q-btn dense flat round icon="lens" size="8.5px" color="yellow" />
