b4ac01425be4bb6d1ddf5faf912047fcca8668af
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-03-27 21:48:50 +0100

    move application management to a store

---
+++ src/stores/application.ts
@@ -0,0 +1,38 @@
+import { defineStore } from 'pinia'
+import { shallowRef } from 'vue'
+import type Application from '@/types/Application.ts'
+
+export const useApplicationStore = defineStore('application', () => {
+  const openedApps = shallowRef<Array<Application>>([])
+
+  const openApp = (app: Application) => {
+    const foundApp = openedApps.value.find((a) => a.name === app.name)
+    if (foundApp) {
+      focusApp(foundApp.name)
+    } else {
+      openedApps.value.push(app)
+    }
+  }
+
+  const closeApp = (name: string) => {
+    openedApps.value = openedApps.value.filter((app) => app.name !== name)
+  }
+
+  const focusApp = (name: string) => {
+    const foundApp = openedApps.value.find((app) => app.name === name)
+    if (foundApp) {
+      openedApps.value = [...openedApps.value.filter((app) => app.name !== name), foundApp]
+    }
+  }
+
+  const isAppFocused = (name: string) =>
+    openedApps.value.length > 0 && openedApps.value[openedApps.value.length - 1].name === name
+
+  return {
+    openedApps,
+    openApp,
+    closeApp,
+    focusApp,
+    isAppFocused,
+  }
+})
