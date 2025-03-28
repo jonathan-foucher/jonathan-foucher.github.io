fa64ecd99aede9cc0b3a7b686ba212a59b69df06
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-03-28 17:33:40 +0100

    add glowing effect on turn on button

--- src/components/ShutdownScreen.vue
+++ src/components/ShutdownScreen.vue
@@ -7,7 +7,7 @@ const { turnOn } = powerStore
 
 const isTurningOn = ref<boolean>(false)
 
-const startBooting = () => {
+const startBootingUp = () => {
   isTurningOn.value = true
   setTimeout(() => turnOn(), 800)
 }
@@ -15,19 +15,49 @@ const startBooting = () => {
 
 <template>
   <div class="vertical-center">
-    <q-btn flat stack outline no-caps rounded :color="isTurningOn ? 'positive' : 'white'" @click="startBooting">
-      <q-icon name="power_settings_new" size="5em" />
-      <span class="text-white">Turn ON</span>
+    <q-btn
+      flat
+      stack
+      outline
+      no-caps
+      rounded
+      size="2.5em"
+      :color="isTurningOn ? 'positive' : 'white'"
+      @click="startBootingUp"
+    >
+      <q-icon name="power_settings_new" size="3em" :class="isTurningOn ? 'glowing-positive' : 'glowing-white'" />
+      <span class="text-white glowing-white">Turn ON</span>
     </q-btn>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@import '@/quasar-variables.sass';
+
 .vertical-center {
   position: absolute;
   top: 45%;
   left: 50%;
   -ms-transform: translateY(-50%) translateX(-50%);
   transform: translateY(-50%) translateX(-50%);
+}
+
+.glowing-white {
+  --glowing-color: white;
+  animation: glow 1200ms ease-in-out infinite alternate;
+}
+
+.glowing-positive {
+  --glowing-color: #{$positive};
+  animation: glow 1200ms ease-in-out infinite alternate;
+}
+
+@keyframes glow {
+  from {
+    text-shadow: 0 0 4px var(--glowing-color);
+  }
+  to {
+    text-shadow: 0 0 14px var(--glowing-color);
+  }
 }
 </style>
