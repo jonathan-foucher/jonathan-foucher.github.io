b63c59fe63de109256f60d848df142f168a5df5d
Jonathan Foucher <46852393+jonathan-foucher@users.noreply.github.com>
2025-03-29 13:01:03 +0100

    add report a bug option in help menu

--- src/components/toolbar/menus/HelpMenu.vue
+++ src/components/toolbar/menus/HelpMenu.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 import { useApplicationStore } from '@/stores/application.ts'
+import { useRedirection } from '@/composables/redirection.ts'
 import { ApplicationList } from '@/types/ApplicationList.ts'
 
 const { t } = useI18n()
 
 const applicationStore = useApplicationStore()
 const { openApp } = applicationStore
+
+const { openInNewTab, getGithubProjectUrl } = useRedirection()
+const reportBugUrl = `${getGithubProjectUrl()}/issues`
 </script>
 
 <template>
@@ -14,10 +18,17 @@ const { openApp } = applicationStore
     <div class="help-text">
       {{ t('global.help') }}
     </div>
-    <q-menu auto-close>
+    <q-menu auto-close class="disable-select cursor-default">
       <q-card class="q-pa-none">
         <q-item dense class="q-py-sm" clickable @click="openApp(ApplicationList.ABOUT_PROJECT)">
           {{ t('about-project.app-title') }}
+        </q-item>
+        <q-separator />
+        <q-item dense class="q-py-sm" clickable @click="openInNewTab(reportBugUrl)">
+          <div>
+            {{ t('about-project.report-bug') }}
+            <q-icon name="open_in_new" class="q-pl-xs" />
+          </div>
         </q-item>
       </q-card>
     </q-menu>
