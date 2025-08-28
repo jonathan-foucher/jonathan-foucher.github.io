d5957a368546a99a7b231e1dd53959421880188a
Jonathan Foucher <jonathan-foucher@outlook.fr>
2025-08-28 09:47:33 +0200

    fix rootme redirection

--- src/composables/redirection.ts
+++ src/composables/redirection.ts
@@ -2,7 +2,7 @@ export function useRedirection() {
   const GITHUB_PROFILE_URL = 'https://github.com/jonathan-foucher'
   const CURRENT_PROJECT_NAME = 'jonathan-foucher.github.io'
   const LINKED_IN_PROFILE_URL = 'https://linkedin.com/in/jonathanfoucher/'
-  const ROOT_ME_PROFILE_URL = 'https://root-me.org/Jonathan-608847'
+  const ROOT_ME_PROFILE_URL = 'https://www.root-me.org/Jonathan-608847'
 
   const openInNewTab = (url: string) => {
     open(url, '_blank')
